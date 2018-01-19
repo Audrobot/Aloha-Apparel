8e1906a6f0dbe4b6cd350de0d4067269b873004c
Audra K <audrak@Audras-MacBook-Pro.local>
2018-01-19 15:17:23 -0800

    aloha_apparel_project_1 git log git status ls cd ls pwd

---
+++ script.js
@@ -0,0 +1,37 @@
+$(function() {
+$('.main-carousel').flickity({
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true
+  })
+});
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000, function() {
+        });
+      }
+    }
+  });
+
+$('form').submit(function(event) {
+  event.preventDefault();
+  if ( $('input').val() == '' ) {
+     alert('Please submit a valid email address.');
+  } else {
+     alert('Thanks for subscribing!');
+  }
+});
